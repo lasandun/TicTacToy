6d5f206872478cb5c1ccb8555c2ac58a4cde077a
lasandun <lasandun@gmail.com>
2014-11-23 14:41:51 +0530

    first commit

---
+++ src/tictactoy/TicTacToy.java
@@ -0,0 +1,87 @@
+package tictactoy;
+
+public class TicTacToy {
+    
+    public final int player;
+    public final int free;
+    //int board[][];
+    
+    public TicTacToy() {
+        player = 1;
+        free = 0;
+        //board = new int[3][3];
+    }
+    
+    private boolean isRowFree(int row, int player, int board[][]) {
+        int opponent = player * (-1);
+        for(int col = 0; col < 3; ++col) {
+            if(board[row][col] == opponent) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isColumnFree(int col, int player, int board[][]) {
+        int opponent = player * (-1);
+        for(int row = 0; row < 3; ++row) {
+            if(board[row][col] == opponent) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isDiagonal1Free(int player, int board[][]) {
+        int opponent = player * (-1);
+        for(int i = 0; i < 3; ++i) {
+            if(board[i][i] == opponent) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isDiagonal2Free(int player, int board[][]) {
+        int opponent = player * (-1);
+        for(int i = 0; i < 3; ++i) {
+            if(board[i][2 - i] == opponent) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    int winningPossibilities(int player, int board[][]) {
+        int count = 0;
+        for(int i = 0; i < 3; ++i) {
+            if(isColumnFree(i, player, board)) count++;
+            if(isRowFree(i, player, board))    count++;
+        }
+        if(isDiagonal1Free(player, board))     count++;
+        if(isDiagonal2Free(player, board))     count++;
+        
+        return count;
+    }
+
+    private int evaluation(int board[][]) {
+        int opponent = player * (-1);
+        int val = winningPossibilities(player, board) - winningPossibilities(opponent, board);
+        return val;
+    }
+    
+    int[] bestMove(int player, int board[][]) {
+        int bestMove[] = new int[2];
+        for(int r = 0; r < 3; ++r) {
+            for(int c = 0; c < 3; ++c) {
+                
+            }
+        }
+        
+        return bestMove;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
